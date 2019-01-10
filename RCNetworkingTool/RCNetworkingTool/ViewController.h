070053e2f622860c9728ea615c678e06141c5cd1
孙承秀 <>
2019-01-10 14:54:09 +0800

    init project

---
+++ RCNetworkingTool/RCNetworkingTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RCNetworkingTool
+//
+//  Created by 孙承秀 on 2019/1/10.
+//  Copyright © 2019 RongCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RCNetworkingTool/RCNetworkingTool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RCNetworkingTool
+//
+//  Created by 孙承秀 on 2019/1/10.
+//  Copyright © 2019 RongCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
